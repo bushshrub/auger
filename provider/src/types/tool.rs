90f76b084e1c6a104d253ec948cf432e774c0d02
Robert <9291792+bushshrub@users.noreply.github.com>
2026-06-20 21:51:02 -0400

    improve doc in provider crate

--- provider/src/types/tool.rs
+++ provider/src/types/tool.rs
@@ -2,15 +2,21 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone)]
 pub struct ToolDefinition {
+    /// The name of the tool
     pub name: String,
+    /// An optional description of the tool for the model.
     pub description: Option<String>,
+    /// The parameters that the tool expects, as a JSON schema object.
     pub parameters: serde_json::Value,
 }
 
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ToolCall {
+    /// The unique ID of this tool call, used for approval and result reporting
     pub id: String,
+    /// The name of the tool to call, which should match one of the tools provided in the request
     pub name: String,
+    /// The arguments to call the tool with. Generally this is a JSON string.
     pub arguments: String,
 }
