e57fe8efbb94aded3a0a56374dc412d4490eeb97
Robert <9291792+bushshrub@users.noreply.github.com>
2026-07-15 20:27:07 -0400

    traces: build out the session record struct

--- auger-traces/src/lib.rs
+++ auger-traces/src/lib.rs
@@ -1,7 +1,26 @@
+use std::path::PathBuf;
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SessionRecord {
+    header: SessionHeader,
+    events: Vec<EventRecord>
+}
+
+impl SessionRecord {
+    pub fn new(session_id: Uuid, cwd: PathBuf, model: ModelInfo) -> Self {
+        Self {
+            header: SessionHeader::new(session_id, cwd, model),
+            events: Vec::new()
+        }
+    }
+
+}
+
+/// A harness level event record.
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EventRecord {
     /// The ID of this event
     id: Uuid,
@@ -14,16 +33,71 @@ pub struct EventRecord {
     event: Event
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SessionHeader {
+    version: u32,
+    session_id: Uuid,
+    created_at: DateTime<Utc>,
+    cwd: PathBuf,
+    model: ModelInfo,
+}
+
+impl SessionHeader {
+    pub(crate) fn new(session_id: Uuid, cwd: PathBuf, model: ModelInfo) -> Self {
+        Self {
+            version: 1,
+            session_id,
+            created_at: Utc::now(),
+            cwd,
+            model
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ProviderType {
+    OpenAiResponses,
+    OpenAiChatCompletions,
+    AnthropicMessages
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ModelInfo {
+    provider_type: ProviderType,
+}
+
+impl ModelInfo {
+    pub fn new(provider_type: ProviderType) -> Self {
+        Self { provider_type }
+    }
+}
+
+impl EventRecord {
+    /// Create a new event record with the timestamp being the time
+    /// function was invoked.
+    pub fn new(parent_id: Option<Uuid>, seq: u64, event: Event) -> Self {
+        let timestamp = Utc::now();
+        let id = Uuid::new_v7(timestamp.into());
+        Self {
+            id,
+            parent_id,
+            seq,
+            timestamp,
+            event
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum InputContent {
-    UserMessage(String),
+    UserMessage(String), // For now only string messages
     ToolCallResult {
         tool_call_id: String,
         content: Vec<ToolData>
-    }// For now only string messages
+    }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Event {
     InputMessage {
@@ -53,31 +127,31 @@ pub enum Event {
 
 /// Data returned by tool.
 /// Currently only text, image will eventually come.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum ToolData {
     Text(String)
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum ToolDecision {
     Approved,
     Denied
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum ToolCallStatus {
     Success,
     Denied,
     Error
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum AuthorizationSource {
     User,
     Policy
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum AssistantContent {
     Text(String),
     ToolCallRequest {
@@ -87,7 +161,7 @@ pub enum AssistantContent {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum AssistantStatus {
     Completed,
     Interrupted,
